24b9080c62757b9cf0e12aa6362043293799a14e
Mikhail Koshelev <koshelev.mikhail@outlook.com>
2026-03-22 20:13:53 +0200

    feat: refactor MaterialsCard to use DragDropProvider and remove SortableList component

--- src/ui/MaterialsCard.tsx
+++ src/ui/MaterialsCard.tsx
@@ -9,12 +9,17 @@ import {
 import {
   addMaterial,
   removeMaterial,
+  setMaterials,
   updateMaterial,
   useModel,
 } from "@/modelStore";
 import { GripVertical, Plus } from "lucide-react";
-import { SortableList, SortableListItem } from "./SortableList";
 import { Input } from "@/components/ui/input";
+import type { GroundMaterial } from "@/types";
+import { DragDropProvider } from "@dnd-kit/react";
+import { useSortable } from "@dnd-kit/react/sortable";
+import { RestrictToVerticalAxis } from "@dnd-kit/abstract/modifiers";
+import { move } from "@dnd-kit/helpers";
 
 export function MaterialsCard() {
   const materials = useModel((state) => state.materials);
@@ -26,10 +31,6 @@ export function MaterialsCard() {
       weight: 18,
       phi: 35,
     });
-  };
-
-  const handleRemoveMaterial = (id: string) => {
-    removeMaterial(id);
   };
 
   return (
@@ -55,52 +56,81 @@ export function MaterialsCard() {
           </div>
           <div></div>
         </div>
-        <SortableList ids={materials.map((m) => m.id)}>
+        <DragDropProvider
+          onDragEnd={(e) => {
+            setMaterials(move(materials, e));
+          }}
+          modifiers={[RestrictToVerticalAxis]}
+        >
           <div className="flex flex-col gap-2">
-            {materials.map((material) => (
-              <SortableListItem key={material.id} id={material.id}>
-                <div className="grid grid-cols-[1.5rem_1fr_4.5rem_4.5rem_2rem] gap-2 items-center">
-                  <GripVertical className="cursor-grab text-muted-foreground w-5 h-5 mx-auto" />
-                  <Input
-                    className="h-8"
-                    value={material.name}
-                    onChange={(e) =>
-                      updateMaterial(material.id, { name: e.target.value })
-                    }
-                  />
-                  <Input
-                    type="number"
-                    className="h-8"
-                    value={material.weight}
-                    onChange={(e) =>
-                      updateMaterial(material.id, {
-                        weight: Number(e.target.value),
-                      })
-                    }
-                  />
-                  <Input
-                    type="number"
-                    className="h-8"
-                    value={material.phi}
-                    onChange={(e) =>
-                      updateMaterial(material.id, {
-                        phi: Number(e.target.value),
-                      })
-                    }
-                  />
-                  <Button
-                    size="icon-sm"
-                    variant="destructive"
-                    onClick={() => handleRemoveMaterial(material.id)}
-                  >
-                    <Plus className="rotate-45" />
-                  </Button>
-                </div>
-              </SortableListItem>
+            {materials.map((material, index) => (
+              <MaterialItem
+                key={material.id}
+                material={material}
+                index={index}
+              />
             ))}
           </div>
-        </SortableList>
+        </DragDropProvider>
       </CardContent>
     </Card>
+  );
+}
+
+function MaterialItem({
+  material,
+  index,
+}: {
+  material: GroundMaterial;
+  index: number;
+}) {
+  const { ref, handleRef } = useSortable({
+    id: material.id,
+    index: index,
+  });
+
+  const handleRemoveMaterial = (id: string) => {
+    removeMaterial(id);
+  };
+
+  return (
+    <div
+      ref={ref}
+      className="grid grid-cols-[1.5rem_1fr_4.5rem_4.5rem_2rem] gap-2 items-center"
+    >
+      <GripVertical
+        ref={handleRef}
+        className="cursor-grab text-muted-foreground w-5 h-5 mx-auto"
+      />
+      <Input
+        value={material.name}
+        onChange={(e) => updateMaterial(material.id, { name: e.target.value })}
+      />
+      <Input
+        type="number"
+        value={material.weight}
+        onChange={(e) =>
+          updateMaterial(material.id, {
+            weight: Number(e.target.value),
+          })
+        }
+      />
+      <Input
+        type="number"
+        value={material.phi}
+        onChange={(e) =>
+          updateMaterial(material.id, {
+            phi: Number(e.target.value),
+          })
+        }
+      />
+      <Button
+        size="icon-sm"
+        variant="destructive"
+        onClick={() => handleRemoveMaterial(material.id)}
+      >
+        <Plus className="rotate-45" />
+      </Button>
+    </div>
   );
 }
